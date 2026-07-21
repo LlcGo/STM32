fe2a30bb3c8ea0a503eb6f93d725e0f913b0813b
lc <1641767724@qq.com>
2026-07-21 21:34:18 +0800

    1、输入捕获

---
+++ 6-3输入捕获测评了(2026-07-21)/User/main.c
@@ -0,0 +1,20 @@
+#include "stm32f10x.h"                  // Device header
+#include "Delay.h"
+#include "OLED.h"
+#include "PWM.h"
+#include "IC.h"
+
+
+int main(void)
+{
+	OLED_Init();
+	PWM_Init();
+	IC_Init();
+	
+	PWM_SetPrescaler(720-1);
+	PWM_SetCompare1(50);
+	
+	while(1){
+	  OLED_ShowNum(1,1,IC_GetFreq(),5);
+	}
+}
