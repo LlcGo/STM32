e47bdf6ac465d9c666e254d2a205ccff1bd0c34b
lc <1641767724@qq.com>
2026-07-27 20:48:36 +0800

    1、获取ADC转换后的值

--- 7-1 ADC采样单通道(2026-07-23)/Hardware/AD.h
+++ 7-1 ADC采样单通道(2026-07-23)/Hardware/AD.h
@@ -2,5 +2,6 @@
 #define __AD_H
 
 void AD_Init(void);
+AD_GetValue(void);
 
 #endif
--- 7-1 ADC采样单通道(2026-07-23)/User/main.c
+++ 7-1 ADC采样单通道(2026-07-23)/User/main.c
@@ -1,18 +1,16 @@
 #include "stm32f10x.h"                  // Device header
 #include "Delay.h"
 #include "OLED.h"
+#include "AD.h"
 
+uint8_t ADC_Value;
 
 int main(void)
 {
 	OLED_Init();
-	OLED_ShowChar(1,1,'A');
-	OLED_ShowNum(2,1,12345,5);
-	OLED_ShowString(1,3,"HelloWorld!");
-	OLED_ShowSignedNum(2,7,-66,2);
-	OLED_ShowHexNum(3,1,0xAA55,4);
-	OLED_ShowBinNum(4,1,0xAA55,16);
+	AD_Init();
+	OLED_ShowNum(1,1,ADC_Value,5);   
 	while(1){
-	
+		ADC_Value = AD_GetValue();
 	}
 }
