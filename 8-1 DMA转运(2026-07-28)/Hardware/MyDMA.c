f5e62c8f67817efa78880a09f831117b00bbcdea
lc <1641767724@qq.com>
2026-07-29 21:02:56 +0800

    1、DMA转运封装

--- 8-1 DMA转运(2026-07-28)/Hardware/MyDMA.c
+++ 8-1 DMA转运(2026-07-28)/Hardware/MyDMA.c
@@ -1,7 +1,11 @@
 #include "stm32f10x.h"                  // Device header
 
+
+uint32_t MYDMA_Size;
+
 void MYDMA_Init(uint32_t Addr1,uint32_t Addr2,uint32_t Size)
 {
+	MYDMA_Size = Size;
 	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1,ENABLE);
 	
 	DMA_Channel_TypeDef DMA_ChannelStruct;
@@ -24,4 +28,13 @@ void MYDMA_Init(uint32_t Addr1,uint32_t Addr2,uint32_t Size)
 
 	DMA_Init(DMA1_Channel1,&DMA_InitTypeStruct);
 	DMA_Cmd(DMA1_Channel1,ENABLE);
+}
+
+void MYDMA_Transfer(void)
+{
+	DMA_Cmd(DMA1_Channel1,DISABLE);
+	DMA_SetCurrDataCounter(DMA1_Channel1,MYDMA_Size);
+	DMA_Cmd(DMA1_Channel1,ENABLE);
+	while(DMA_GetITStatus(DMA1_IT_TC1) == RESET);
+	DMA_ClearFlag(DMA1_IT_TC1);
 }
--- 8-1 DMA转运(2026-07-28)/Hardware/MyDMA.h
+++ 8-1 DMA转运(2026-07-28)/Hardware/MyDMA.h
@@ -2,5 +2,6 @@
 #define __MYDMA_H
 
 void MYDMA_Init(uint32_t Addr1,uint32_t Addr2,uint32_t Size);
+void MYDMA_Transfer(void);
 
 #endif
