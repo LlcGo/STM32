d7777fb2280ec3c59819dd4a777d6e1761fa05cf
lc <1641767724@qq.com>
2026-08-18 21:12:16 +0800

    1、PAO外部时钟定时器

--- 5-2 定时器外部时钟(2026-07-16)/Hardware/Timer.c
+++ 5-2 定时器外部时钟(2026-07-16)/Hardware/Timer.c
@@ -9,18 +9,18 @@ void Timer_Init(void){
 	RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOA,ENABLE);
 	
 	GPIO_InitTypeDef GPIO_InitStructure;
-	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IPU;
+	GPIO_InitStructure.GPIO_Mode = GPIO_Mode_IN_FLOATING;
 	GPIO_InitStructure.GPIO_Pin = GPIO_Pin_0;
 	GPIO_InitStructure.GPIO_Speed = GPIO_Speed_50MHz;
 	GPIO_Init(GPIOA,&GPIO_InitStructure);
 	
-	TIM_ETRClockMode2Config(TIM2,TIM_ExtTRGPSC_OFF,TIM_ExtTRGPolarity_NonInverted,0x00);
+	TIM_ETRClockMode2Config(TIM2, TIM_ExtTRGPSC_OFF,TIM_ExtTRGPolarity_NonInverted, 0x0);
 	
 	// 初始化时基单元
 	TIM_TimeBaseInitTypeDef TIM_TimeBaseInitStruct;
 	TIM_TimeBaseInitStruct.TIM_ClockDivision = TIM_CKD_DIV1;
 	TIM_TimeBaseInitStruct.TIM_CounterMode = TIM_CounterMode_Up;
-	TIM_TimeBaseInitStruct.TIM_Period = 100-1;
+	TIM_TimeBaseInitStruct.TIM_Period = 10-1;
 	TIM_TimeBaseInitStruct.TIM_Prescaler = 1-1;
 	TIM_TimeBaseInitStruct.TIM_RepetitionCounter = 0;
 	TIM_TimeBaseInit(TIM2,&TIM_TimeBaseInitStruct);
@@ -49,11 +49,11 @@ uint16_t Timer_GetCounter(void)
 	return TIM_GetCounter(TIM2);
 }
 
-//void TIM2_IRQHandler(void)
-//{
-//	if (TIM_GetITStatus(TIM2,TIM_IT_Update) == SET)
-//	{
-//		Num++;
-//		TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
-//	}
-//}
+void TIM2_IRQHandler(void)
+{
+	if (TIM_GetITStatus(TIM2,TIM_IT_Update) == SET)
+	{
+		Num++;
+		TIM_ClearITPendingBit(TIM2,TIM_IT_Update);
+	}
+}
