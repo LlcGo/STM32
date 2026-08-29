9c15fb24edf603f12a9abfc498dda1e30c753156
lc <1641767724@qq.com>
2026-08-29 21:35:26 +0800

    1、DMA转运

--- 8-1 DMA转运(2026-07-28)/Hardware/MyDMA.c
+++ 8-1 DMA转运(2026-07-28)/Hardware/MyDMA.c
@@ -5,36 +5,28 @@ uint32_t MYDMA_Size;
 
 void MYDMA_Init(uint32_t Addr1,uint32_t Addr2,uint32_t Size)
 {
-	MYDMA_Size = Size;
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1,ENABLE);
+	DMA_InitTypeDef DMA_InitStruct;
+	DMA_InitStruct.DMA_BufferSize = Size;
+	DMA_InitStruct.DMA_DIR = DMA_DIR_PeripheralSRC; // 外设作为数据传输的来源
+	DMA_InitStruct.DMA_Mode = DMA_Mode_Normal;
 	
-	DMA_Channel_TypeDef DMA_ChannelStruct;
-	DMA_InitTypeDef DMA_InitTypeStruct;
+	DMA_InitStruct.DMA_PeripheralBaseAddr = Addr1;
+	DMA_InitStruct.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
+	DMA_InitStruct.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
 	
-	DMA_InitTypeStruct.DMA_PeripheralBaseAddr = Addr1;
-	DMA_InitTypeStruct.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
-	DMA_InitTypeStruct.DMA_PeripheralInc = DMA_PeripheralInc_Enable;
+	DMA_InitStruct.DMA_MemoryBaseAddr = Addr2;
+	DMA_InitStruct.DMA_MemoryDataSize = DMA_PeripheralDataSize_Byte;
+	DMA_InitStruct.DMA_MemoryInc = DMA_PeripheralInc_Disable;
 	
+	DMA_InitStruct.DMA_M2M = DMA_M2M_Enable;
 	
-	DMA_InitTypeStruct.DMA_MemoryBaseAddr = Addr2;
-	DMA_InitTypeStruct.DMA_MemoryDataSize = DMA_MemoryDataSize_Byte;
-	DMA_InitTypeStruct.DMA_MemoryInc = DMA_MemoryInc_Enable;
-	
-	DMA_InitTypeStruct.DMA_Priority = DMA_Priority_VeryHigh;
-	DMA_InitTypeStruct.DMA_Mode = DMA_Mode_Normal;
-	DMA_InitTypeStruct.DMA_BufferSize = Size;
-	DMA_InitTypeStruct.DMA_DIR = DMA_DIR_PeripheralSRC;
-	DMA_InitTypeStruct.DMA_M2M = DMA_M2M_Enable;
+	DMA_InitStruct.DMA_Priority = DMA_Priority_VeryHigh;
+	DMA_Init(DMA1_Channel1,&DMA_InitStruct);
 
-	DMA_Init(DMA1_Channel1,&DMA_InitTypeStruct);
-	DMA_Cmd(DMA1_Channel1,ENABLE);
 }
 
 void MYDMA_Transfer(void)
 {
-	DMA_Cmd(DMA1_Channel1,DISABLE);
-	DMA_SetCurrDataCounter(DMA1_Channel1,MYDMA_Size);
 	DMA_Cmd(DMA1_Channel1,ENABLE);
-	while(DMA_GetITStatus(DMA1_IT_TC1) == RESET);
-	DMA_ClearFlag(DMA1_IT_TC1);
+	while(DMA_GetFlagStatus(DMA1_FLAG_TC1) == RESET);
 }
