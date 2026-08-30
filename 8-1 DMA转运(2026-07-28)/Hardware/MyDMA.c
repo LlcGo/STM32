78a5eaaeca28a798a6ab161e37a703aa608319ad
lc <1641767724@qq.com>
2026-08-30 20:45:32 +0800

    1、DMA转运

--- 8-1 DMA转运(2026-07-28)/Hardware/MyDMA.c
+++ 8-1 DMA转运(2026-07-28)/Hardware/MyDMA.c
@@ -1,32 +1,39 @@
 #include "stm32f10x.h"                  // Device header
 
-
 uint32_t MYDMA_Size;
 
 void MYDMA_Init(uint32_t Addr1,uint32_t Addr2,uint32_t Size)
 {
+	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_DMA1,ENABLE);
+	
 	DMA_InitTypeDef DMA_InitStruct;
 	DMA_InitStruct.DMA_BufferSize = Size;
+	MYDMA_Size = Size;
 	DMA_InitStruct.DMA_DIR = DMA_DIR_PeripheralSRC; // 外设作为数据传输的来源
 	DMA_InitStruct.DMA_Mode = DMA_Mode_Normal;
 	
 	DMA_InitStruct.DMA_PeripheralBaseAddr = Addr1;
 	DMA_InitStruct.DMA_PeripheralDataSize = DMA_PeripheralDataSize_Byte;
-	DMA_InitStruct.DMA_PeripheralInc = DMA_PeripheralInc_Disable;
+	DMA_InitStruct.DMA_PeripheralInc = DMA_PeripheralInc_Enable;
 	
 	DMA_InitStruct.DMA_MemoryBaseAddr = Addr2;
 	DMA_InitStruct.DMA_MemoryDataSize = DMA_PeripheralDataSize_Byte;
-	DMA_InitStruct.DMA_MemoryInc = DMA_PeripheralInc_Disable;
+	DMA_InitStruct.DMA_MemoryInc = DMA_MemoryInc_Enable;
 	
 	DMA_InitStruct.DMA_M2M = DMA_M2M_Enable;
 	
 	DMA_InitStruct.DMA_Priority = DMA_Priority_VeryHigh;
 	DMA_Init(DMA1_Channel1,&DMA_InitStruct);
-
+	
+    DMA_Cmd(DMA1_Channel1,DISABLE);
 }
 
 void MYDMA_Transfer(void)
 {
+	DMA_Cmd(DMA1_Channel1,DISABLE);
+	DMA_SetCurrDataCounter(DMA1_Channel1,MYDMA_Size);
 	DMA_Cmd(DMA1_Channel1,ENABLE);
+	
 	while(DMA_GetFlagStatus(DMA1_FLAG_TC1) == RESET);
+	DMA_ClearFlag(DMA1_FLAG_TC1);
 }
--- 8-1 DMA转运(2026-07-28)/User/main.c
+++ 8-1 DMA转运(2026-07-28)/User/main.c
@@ -3,19 +3,53 @@
 #include "OLED.h"
 #include "MyDMA.h"
 
-uint8_t DataA[] = {0X01};
-uint8_t DataB[] = {0};
+uint8_t DataA[] = {0X01,0x02,0x03,0x04};
+uint8_t DataB[] = {0x00,0x00,0x00,0x00};
 
 int main(void)
 {
-	OLED_Init();
+	/*模块初始化*/
+	OLED_Init();				//OLED初始化
 	
-	OLED_ShowHexNum(1,1,DataA[0],1);
-	OLED_ShowHexNum(2,1,DataB[0],1);
-	MYDMA_Init((uint32_t)DataA,(uint32_t)DataB,1);
-	OLED_ShowHexNum(3,1,DataA[0],1);
-	OLED_ShowHexNum(4,1,DataB[0],1);
-	while(1){
+	MYDMA_Init((uint32_t)DataA, (uint32_t)DataB, 4);	//DMA初始化，把源数组和目的数组的地址传入
 	
+	/*显示静态字符串*/
+	OLED_ShowString(1, 1, "DataA");
+	OLED_ShowString(3, 1, "DataB");
+	
+	/*显示数组的首地址*/
+	OLED_ShowHexNum(1, 8, (uint32_t)DataA, 8);
+	OLED_ShowHexNum(3, 8, (uint32_t)DataB, 8);
+		
+	while (1)
+	{
+		DataA[0] ++;		//变换测试数据
+		DataA[1] ++;
+		DataA[2] ++;
+		DataA[3] ++;
+		
+		OLED_ShowHexNum(2, 1, DataA[0], 2);		//显示数组DataA
+		OLED_ShowHexNum(2, 4, DataA[1], 2);
+		OLED_ShowHexNum(2, 7, DataA[2], 2);
+		OLED_ShowHexNum(2, 10, DataA[3], 2);
+		OLED_ShowHexNum(4, 1, DataB[0], 2);		//显示数组DataB
+		OLED_ShowHexNum(4, 4, DataB[1], 2);
+		OLED_ShowHexNum(4, 7, DataB[2], 2);
+		OLED_ShowHexNum(4, 10, DataB[3], 2);
+		
+		Delay_ms(1000);		//延时1s，观察转运前的现象
+		
+		MYDMA_Transfer();	//使用DMA转运数组，从DataA转运到DataB
+		
+		OLED_ShowHexNum(2, 1, DataA[0], 2);		//显示数组DataA
+		OLED_ShowHexNum(2, 4, DataA[1], 2);
+		OLED_ShowHexNum(2, 7, DataA[2], 2);
+		OLED_ShowHexNum(2, 10, DataA[3], 2);
+		OLED_ShowHexNum(4, 1, DataB[0], 2);		//显示数组DataB
+		OLED_ShowHexNum(4, 4, DataB[1], 2);
+		OLED_ShowHexNum(4, 7, DataB[2], 2);
+		OLED_ShowHexNum(4, 10, DataB[3], 2);
+
+		Delay_ms(1000);		//延时1s，观察转运后的现象
 	}
 }
